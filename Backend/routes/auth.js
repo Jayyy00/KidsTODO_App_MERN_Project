bf481cdc06f3f3b91586b0ecf8e3fd1464bad4b9
J.A Jayani Jayasinghe <99647114+Jayyy00@users.noreply.github.com>
2024-01-19 02:32:25 +0530

    third commit

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -21,6 +21,31 @@ router.post("/register",async(req,res)=>{
 });
 
 
-//LOG IN
+//Sign IN
+router.post("/signin",async(req,res)=>{
+  try{
+    const user = await User.findOne({email:req.body.email});
+    //Check if the user already exists
+  
+
+    if (!user) {
+      res.status(400).json({ message: "Please Sing Up First" });
+    }
+    
+    const ispasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      user.password
+      );
+      if (!ispasswordCorrect) {
+        res.status(400).json({ message: "Password Is Not Correct" });
+      }
+      const{password,...others}= user._doc;
+      res.status(200).json({others});
+  }catch(error){
+    //console.log(error);
+    res.status(400).json({message:"User Alredy Exists"});
+}
+});
+
 
 module.exports = router;
